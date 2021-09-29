3af8f2446e9a8f419ea6187db0b2a47dd054c485
ismaelmendesbueno <yasuobestgm@gmail.com>
2021-09-29 20:54:13 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,11 @@
+/* var botao = document.querySelector("#botao")
+
+botao.addEventListener("click", function (){
+    var txt = document.querySelector("#txt")
+    var menu = document.querySelector(".menu")
+
+    menu.classList.add("hide")
+    txt.classList.add("show")
+})
+
+*/
