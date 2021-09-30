c46e7199f2c239a2bc8bd9bd2e4fa582782dea19
ismaelmendesbueno <yasuobestgm@gmail.com>
2021-09-30 14:57:28 -0300

    js

---
+++ js.js
@@ -0,0 +1,19 @@
+var botao = document.querySelector("#botao")
+
+botao.addEventListener("click", function(){
+    var nav = document.querySelector(".menu")
+    nav.classList.add("hide")
+
+    var inp = document.querySelector(".inp")
+    inp.classList.remove("hide")
+})
+
+var fechar = document.querySelector("#fechar")
+
+fechar.addEventListener("click", function(){
+    var nav = document.querySelector(".menu")
+    nav.classList.remove("hide")
+
+    var inp = document.querySelector(".inp")
+    inp.classList.add("hide")
+})
